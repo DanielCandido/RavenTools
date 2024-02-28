56c06286dc24ee6a22f59bb8fed188658b090525
DanielCandido <danielfelipec18@hotmail.com>
2024-02-28 10:33:58 -0300

    ci: adding https in baseUrl

--- App/src/services/api.ts
+++ App/src/services/api.ts
@@ -2,7 +2,7 @@ import getEnv from "@/helpers/getEnv";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: getEnv("NEXT_PUBLIC_API_BASE_URL", "http://rootech.site/api"),
+  baseURL: getEnv("NEXT_PUBLIC_API_BASE_URL", "https://rootech.site/api"),
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
