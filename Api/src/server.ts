8ba85e98a05331c6e0a7a5e61181a7ec2c402547
DanielCandido <danielfelipec18@hotmail.com>
2024-03-10 14:05:07 -0300

    feat: adding cors config

--- Api/src/server.ts
+++ Api/src/server.ts
@@ -9,6 +9,14 @@ import router from "@src/router";
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  next();
+});
 app.use(cors());
 app.use(express.static(__dirname + "public"));
 
